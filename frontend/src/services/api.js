6014f4195edd457f5450f546d787db1526c6f3dc
sparsh-4066 <sparshuser1234@gmail.com>
2026-07-25 20:48:37 +0530

    Revert deployment changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://sentiment-analysis-and-automated.onrender.com/api",
+  baseURL: "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
